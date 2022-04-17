b0d62ed7511590943ca5101a16c1e5d3bc66b49e
Martin Benes <benesm44@fit.cvut.cz>
2022-04-17 11:36:02 +0200

    main: add mpu6050 set-up

--- src/main/main.ino
+++ src/main/main.ino
@@ -1,14 +1,29 @@
 #include <LiquidCrystal.h>
 #include "Wire.h"
+#include <MPU6050_light.h>
+
+//mpu set-up
+MPU6050 mpu(Wire);
 
 //pins for lcd
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 void setup() {
+  //initialize serial monitor
+  Serial.begin(9600);
+  
   //initialize LCD display
   lcd.begin(16, 2);
   Wire.begin();
+
+  //initialize mpu sensor
+  byte status = mpu.begin();
+  Serial.print(F("MPU6050 status: "));
+  Serial.println(status);
+  while (status != 0) { }
+  //recalculates both gyro and accelerometer offsets
+  mpu.calcOffsets(true, true);
   
 
 }
