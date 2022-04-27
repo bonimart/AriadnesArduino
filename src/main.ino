e737de089992d9f2afe80136b67f7a76400078fc
Martin Benes <benesm44@fit.cvut.cz>
2022-04-27 23:02:40 +0200

    add state logic

--- src/main.ino
+++ src/main.ino
@@ -2,6 +2,11 @@
 #include "Wire.h"
 #include <MPU6050_light.h>
 #include "stack/stack.h"
+#include "stack/stack.cpp"
+
+#define WAIT_TIME 500
+#define END 6
+#define STEP 7
 
 //mpu set-up
 MPU6050 mpu(Wire);
@@ -10,6 +15,9 @@ MPU6050 mpu(Wire);
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
+enum states {MENU, READ, NAVIGATE};
+enum states STATE = MENU;
+
 void setup() {
   //initialize serial monitor
   Serial.begin(9600);
@@ -17,6 +25,10 @@ void setup() {
   //initialize LCD display
   lcd.begin(16, 2);
   Wire.begin();
+
+  //initialize buttons
+  pinMode(END, INPUT_PULLUP);
+  pinMode(STEP, INPUT_PULLUP);
 
   //initialize mpu sensor
   byte status = mpu.begin();
@@ -29,6 +41,68 @@ void setup() {
 
 }
 
-void loop() {
+unsigned long timer = 0;
+bool end;
+bool st;
+Stack angles;
 
+bool risingEdge(int pin, bool val){
+  return !digitalRead(pin) && val == true;
+}
+
+void loop() {
+  switch (STATE)
+  {
+    case MENU:
+      lcd.clear();
+      lcd.print("Press button!");
+      if(risingEdge(STEP, st)){
+        STATE = READ;
+      }
+      end = digitalRead(END);
+      st = digitalRead(STEP);
+      break;
+      
+    
+    case READ:
+      lcd.clear();
+      lcd.print("Reading");
+      if(risingEdge(END, end)){
+        STATE = NAVIGATE;
+      }
+      else if(risingEdge(STEP, st)){
+        //make a step
+      }
+      end = digitalRead(END);
+      st = digitalRead(STEP);
+      break;
+      
+    case NAVIGATE:
+      lcd.clear();
+      lcd.print("Navigating");
+      if(risingEdge(END, end)){
+        STATE = MENU;
+      }
+      else if(risingEdge(STEP, st)){
+        //make a step
+      }
+      end = digitalRead(END);
+      st = digitalRead(STEP);
+      break;
+  }
+  delay(200);
+  //update sensor
+  /*
+  mpu.update();
+
+  //write info on LCD
+  if(millis() - timer > WAIT_TIME){
+    timer = millis();
+    lcd.clear();
+    lcd.print("Angle: ");
+    lcd.print(mpu.getAngleZ(), 0);
+    lcd.print(" DEG");
+
+  }
+  */
 }
